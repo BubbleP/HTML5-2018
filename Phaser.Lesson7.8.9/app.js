eeec5ea9c221fbf9d7eeccaee9f2582936b0beab
BubbleP <naqvia1@dc.edu.hk>
2018-03-18 17:14:50 +0800

    AC302_Sat

--- Phaser.Lesson7.8.9/app.js
+++ Phaser.Lesson7.8.9/app.js
@@ -10,7 +10,6 @@ function preload(){
 	game.load.image('star', 'assets/star.png')
 	game.load.spritesheet('dude', 'assets/dude.png', 32, 48)
 	game.load.spritesheet('baddie', 'assets/baddie.png', 32, 32)
-	game.load.spritesheet('firstaid','assets/firstaid.png'32,32)
 }
 
 function create(){
